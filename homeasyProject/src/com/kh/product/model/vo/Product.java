4276f3e6b1a62f3e9f19ed319c3d71e1eb78ecfd
jihwanew <comzzone@naver.com>
2021-04-17 18:18:32 +0900

    Product클래스 생성 -지환-
    생성

--- homeasyProject/src/com/kh/product/model/vo/Product.java
+++ homeasyProject/src/com/kh/product/model/vo/Product.java
@@ -1,6 +1,102 @@
 package com.kh.product.model.vo;
 
+import java.sql.Date;
+
 public class Product {
 
 	private int productNo;
+	private int cate2No;
+	private String productName;
+	private String productBrand;
+	private int productPrice;
+	private String productDetail;
+	private Date productDate;
+	private String productStatus;
+	
+	public Product() {}
+
+	public Product(int productNo, int cate2No, String productName, String productBrand, int productPrice,
+			String productDetail, Date productDate, String productStatus) {
+		super();
+		this.productNo = productNo;
+		this.cate2No = cate2No;
+		this.productName = productName;
+		this.productBrand = productBrand;
+		this.productPrice = productPrice;
+		this.productDetail = productDetail;
+		this.productDate = productDate;
+		this.productStatus = productStatus;
+	}
+
+	public int getProductNo() {
+		return productNo;
+	}
+
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
+	}
+
+	public int getCate2No() {
+		return cate2No;
+	}
+
+	public void setCate2No(int cate2No) {
+		this.cate2No = cate2No;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductBrand() {
+		return productBrand;
+	}
+
+	public void setProductBrand(String productBrand) {
+		this.productBrand = productBrand;
+	}
+
+	public int getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(int productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public String getProductDetail() {
+		return productDetail;
+	}
+
+	public void setProductDetail(String productDetail) {
+		this.productDetail = productDetail;
+	}
+
+	public Date getProductDate() {
+		return productDate;
+	}
+
+	public void setProductDate(Date productDate) {
+		this.productDate = productDate;
+	}
+
+	public String getProductStatus() {
+		return productStatus;
+	}
+
+	public void setProductStatus(String productStatus) {
+		this.productStatus = productStatus;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [productNo=" + productNo + ", cate2No=" + cate2No + ", productName=" + productName
+				+ ", productBrand=" + productBrand + ", productPrice=" + productPrice + ", productDetail="
+				+ productDetail + ", productDate=" + productDate + ", productStatus=" + productStatus + "]";
+	}
+	
 }
