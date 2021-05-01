89007882c9576271920fa611e49a519b44695a1c
hamvely <71883439+hamvely@users.noreply.github.com>
2021-05-01 16:25:15 +0900

    오예!!!!!!!!!!!!!!!!!

--- homeasyProject/src/com/kh/store/controller/StoreListServlet.java
+++ homeasyProject/src/com/kh/store/controller/StoreListServlet.java
@@ -18,7 +18,7 @@ import com.kh.store.model.service.StoreService;
  */
 @WebServlet("/list.st")
 public class StoreListServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,28 +28,28 @@ public class StoreListServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		int cate1No = Integer.parseInt(request.getParameter("cate1No"));
-		
-		ArrayList<Product> list = new StoreService().selectList(cate1No);
-		request.setAttribute("list", list);
-		
-		ArrayList<ProductCate> cate2List = new StoreService().selectCate2List(cate1No);
-		request.setAttribute("cate2List", cate2List);
-		
-		request.getRequestDispatcher("views/store/storeListView.jsp").forward(request, response);
-	}
+   /**
+    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+    */
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      
+      int cate1No = Integer.parseInt(request.getParameter("cate1No"));
+      
+      ArrayList<Product> list = new StoreService().selectList(cate1No);
+      request.setAttribute("list", list);
+      
+      ArrayList<ProductCate> cate2List = new StoreService().selectCate2List(cate1No);
+      request.setAttribute("cate2List", cate2List);
+      
+      request.getRequestDispatcher("views/store/storeListView.jsp").forward(request, response);
+   }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+   /**
+    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+    */
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      doGet(request, response);
+   }
 
 }
