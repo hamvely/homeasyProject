bd085937f8d7b7f36df81f9a641e4d466a556365
LIM <71923122+jyu1116@users.noreply.github.com>
2021-06-01 20:45:57 +0900

    Update Review.java

--- homeasyProject/src/com/kh/review/model/vo/Review.java
+++ homeasyProject/src/com/kh/review/model/vo/Review.java
@@ -21,7 +21,6 @@ public class Review {
     
     public Review() {}
     
-    
 
     public Review(int reNo, String reContent, int productNo, int userNo) {
 		super();
@@ -31,8 +30,6 @@ public class Review {
 		this.userNo = userNo;
 	}
 
-
-
 	// 작성자: 홍희나
 	public Review(String name, String brand, String price) {
 		super();
@@ -41,14 +38,11 @@ public class Review {
 		this.price = price;
 	}
 	
-	
 	public Review (String productName, int productNo) {
 		super();
 		this.productName = productName;
 		this.productNo = productNo;
 	}
-
-	
 	
 	public Review(int reNo, int productNo, int userNo, String recontent) {
 		super();
@@ -57,8 +51,6 @@ public class Review {
 		this.userNo = userNo;
 		this.recontent = recontent;
 	}
-
-
 
 	// 작성자: 임지우
 	public Review(int reNo, String productName, String email, String reContent, int reMark) {
@@ -70,11 +62,6 @@ public class Review {
 		this.reMark = reMark;
 	}
 
-	
-	
-	
-
-
 	public Review(String recontent, int productNo, int userNo) {
 		super();
 		this.recontent = recontent;
@@ -83,138 +70,93 @@ public class Review {
 	}
 
 
-
 	public String getName() {
 		return name;
 	}
-
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
-
 	public String getBrand() {
 		return brand;
 	}
-
-
 
 	public void setBrand(String brand) {
 		this.brand = brand;
 	}
 
-
-
 	public String getPrice() {
 		return price;
 	}
-
-
 
 	public void setPrice(String price) {
 		this.price = price;
 	}
 
-
-
 	public String getRecontent() {
 		return recontent;
 	}
-
-
 
 	public void setRecontent(String recontent) {
 		this.recontent = recontent;
 	}
 
-
-
 	public int getReNo() {
 		return reNo;
 	}
-
-
 
 	public void setReNo(int reNo) {
 		this.reNo = reNo;
 	}
 
-
-
 	public String getProductName() {
 		return productName;
 	}
-
-
 
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
 
-
-
 	public String getEmail() {
 		return email;
 	}
-
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
-
 	public String getReContent() {
 		return reContent;
 	}
-
-
 
 	public void setReContent(String reContent) {
 		this.reContent = reContent;
 	}
 
-
-
 	public int getReMark() {
 		return reMark;
 	}
-
-
 
 	public void setReMark(int reMark) {
 		this.reMark = reMark;
 	}
 
-
-
 	public int getProductNo() {
 		return productNo;
 	}
-
-
 
 	public void setProductNo(int productNo) {
 		this.productNo = productNo;
 	}
 
-
-
 	public int getUserNo() {
 		return userNo;
 	}
 
-
-
 	public void setUserNo(int userNo) {
 		this.userNo = userNo;
 	}
-
-
 
 	@Override
 	public String toString() {
@@ -222,11 +164,6 @@ public class Review {
 				+ reNo + ", productName=" + productName + ", email=" + email + ", reContent=" + reContent + ", reMark="
 				+ reMark + ", productNo=" + productNo + ", userNo=" + userNo + "]";
 	}
-	
-	
-
-	
-	
 
 	
 }
